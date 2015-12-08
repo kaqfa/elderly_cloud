8ba81272e8622927bd1019ae530dc1926b3503a1
Fahri Firdausillah <elfaatta@gmail.com>
2015-12-08 15:26:05 +0700

    null true

---
+++ info/migrations/0006_auto_20151208_1524.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('info', '0005_auto_20151208_1111'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='posting',
+            name='owner',
+            field=models.ForeignKey(verbose_name=b'Penulis', blank=True, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
