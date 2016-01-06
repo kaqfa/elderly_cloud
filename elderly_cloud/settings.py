c7905b311a89e253c695368adf717ded087efb24
Fahri Firdausillah <elfaatta@gmail.com>
2016-01-06 13:37:19 +0700

    rework dev server

--- elderly_cloud/settings.py
+++ elderly_cloud/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     STATIC_PATH,
