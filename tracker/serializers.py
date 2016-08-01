9ea98cd82d6064d16718f52f4cc2ca0516e0c5cf
nridwan <xa18ridwan@gmail.com>
2016-08-01 11:56:22 +0700

    Fix

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -6,7 +6,7 @@ from tracker.models import Tracker
 class TrackerSerializer(serializers.ModelSerializer):
     # elder = serializers.PrimaryKeyRelatedField(required=False,read_only=True)
     photo = serializers.ImageField(use_url=True, required=False)
-    modified = serializers.DateField(format='%d/%m/%Y', input_formats=['%d/%m/%Y'], required=False)
+    modified = serializers.DateTimeField(format='%d/%m/%Y', input_formats=['%d/%m/%Y'], required=False)
 
     class Meta:
         model = Tracker
