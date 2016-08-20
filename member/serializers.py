7d60fa604452c420f9fcd3b0f310ab35946609f2
nridwan <xa18ridwan@gmail.com>
2016-08-20 19:05:28 +0700

    +Change phone validation

--- member/serializers.py
+++ member/serializers.py
@@ -42,6 +42,8 @@ class ElderSerializer(serializers.ModelSerializer):
     cared_by = CareGiverSerializer(many=True, read_only=True)
     fullname = serializers.CharField(write_only=True)
     birthday = serializers.DateField(format='%d/%m/%Y', input_formats=['%d/%m/%Y'], required=False)
+    phone = serializers.RegexField(regex='(([0-9]{10,14})|(0[0-9]{2}[0-9]{8}))$',max_length=12, 
+                                    validators=[UniqueValidator(queryset=Elder.objects.all())])
     
     def update(self, instance, validated_data):
         user=instance.user
@@ -74,7 +76,7 @@ class SignupSerializer(serializers.Serializer):
     birthday = serializers.DateField(input_formats=['%d/%m/%Y'], required=False)
     gender = serializers.ChoiceField(choices=GENDER_CHOICES,
                                      default='1')
-    phone = serializers.DecimalField(max_digits=12, decimal_places=0,
+    phone = serializers.RegexField(regex='(([0-9]{10,14})|(0[0-9]{2}[0-9]{8}))$',max_length=12, 
                                     validators=[UniqueValidator(queryset=Elder.objects.all())])
     username = serializers.CharField(validators=unique_val,
                                      required=False)
