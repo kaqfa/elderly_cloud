78c278ad918b3362101a0ade6b35799eb6496112
nridwan <xa18ridwan@gmail.com>
2016-09-16 13:41:12 +0700

    #liat nama

--- member/admin.py
+++ member/admin.py
@@ -12,12 +12,17 @@ class MemberAdmin(admin.ModelAdmin):
 
 
 class ElderAdmin(admin.ModelAdmin):
-    list_display = ['user', 'code', 'address', 'birthday', 'phone', 'num_tracks']
+    list_display = ['user', 'get_nama', 'code', 'address', 'birthday', 'phone', 'num_tracks']
     
     def num_tracks(self, obj):
         return obj.tracker_set.count()
     num_tracks.allow_tags = True
     num_tracks.short_description = ("Jumlah Penelusuran")
+    
+    def get_nama(self, obj):
+        return obj.user.first_name + " " + obj.user.last_name
+    get_nama.allow_tags = True
+    get_nama.short_description = ("Nama")
 
 
 class CareGivingAdmin(admin.ModelAdmin):
